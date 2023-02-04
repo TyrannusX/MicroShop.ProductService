b352d0976fe75a87a137bb8c039e9a0b3231bb1c
Robert Reyes <neorob22@gmail.com>
2023-02-04 16:46:14 -0600

    replaced keycloak with fusionauth and added docker-compose

--- run-microshop.sh
+++ run-microshop.sh
@@ -1,3 +1,3 @@
 docker run -d --hostname product-service --name product-service --network microshop-network -p 5000:5000 microshop-product-service:latest
 docker run -d --hostname my-rabbit --name rabbit-mq-server --network microshop-network -p 5672:5672 -p 15672:15672 rabbitmq:management 
-docker run -d --hostname my-keycloak -p 8082:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:20.0.3 start-dev
+docker run -d --hostname my-keycloak --name keycloak-server --network microshop-network -p 8082:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin -v keycloak-volume quay.io/keycloak/keycloak:20.0.3 start-dev
---
+++ stop-microshop.sh
@@ -0,0 +1,4 @@
+docker stop product-service
+docker stop rabbit-mq-server
+docker stop keycloak-server
+docker container prune -f
